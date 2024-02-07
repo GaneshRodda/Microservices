d761fb608c7b40e00215476d0865a22efd4dd8d7
GaneshRodda <85449873+GaneshRodda@users.noreply.github.com>
2024-02-07 21:37:01 +0530

    Delete currency-exchange-microservice directory

--- currency-exchange-microservice/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(1001,'USD','INR',65,'')
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(1002,'EUR','INR',75,'')
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(1003,'AUD','INR',2,'')
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(1004,'USD','AUD',32,'')
-insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values(1005,'EUR','AUD',38,'')
